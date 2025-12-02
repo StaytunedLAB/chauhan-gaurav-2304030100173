86cd04c59f654a2726c01204a5a20da90353c5ec
gaurav <chauhangauravsinh311@gmail.com>
2025-12-02 11:51:45 +0530

    operater

---
+++ operater.js
@@ -0,0 +1,20 @@
+let x = 1, y = 3;
+alert( y - x );  
+
+alert( 5 % 2 );  
+alert( 8 % 3 ); 
+alert( 8 % 4 ); 
+
+
+alert( 2 ** 2 ); 
+alert( 2 ** 3 );  
+alert( 2 ** 4 );
+
+let apples = "2";
+let oranges = "3";
+
+// both values converted to numbers before the binary plus
+alert( +apples + +oranges ); // 5
+
+// the longer variant
+// alert( Number(apples) + Number(oranges) ); // 5
